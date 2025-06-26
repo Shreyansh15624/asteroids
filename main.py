885c53567359263fbf5605153088d92830d02a48
Shreyansh15624 <shreyanshvajroju@gmail.com>
2025-06-26 04:40:39 +0000

    B: 2nd Commit

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -12,6 +14,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60)/1000
+        print(dt)
 if __name__=="__main__":
     main()
